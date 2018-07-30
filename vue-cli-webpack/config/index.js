2a6402cdbe9b69c2263eea8b166ccdeb907426a7
liulibin <17310566659@163.com>
2018-07-30 16:24:23 +0800

    ss

--- vue-cli-webpack/config/index.js
+++ vue-cli-webpack/config/index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 2000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,//false 默认启动后不打开浏览器
     errorOverlay: true,
     notifyOnErrors: true,
